a1138e5f5c68c3ae8a90be5b73db756555d1d706
Dinko Karamfilov <bgllc91@gmail.com>
2019-03-22 01:49:16 -0500

    new cam3

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,3 +3,7 @@ export default function Projects(){
     <h3>This is my projects page!</h3>
     `;
 }
+
+<p>
+      <img style="-webkit-user-select: none;" src="http://75.132.59.151:8081//" width="595" height="446">
+      </p>
