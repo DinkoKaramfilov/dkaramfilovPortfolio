17871451c7ff2e06a9f10225278e475fe3908399
Dinko Karamfilov <bgllc91@gmail.com>
2019-03-22 02:01:57 -0500

    new cam8

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,9 +3,6 @@ export default function Home(){
       <img src="https://pbs.twimg.com/profile_images/378800000427609721/c50b7445958e266822842f82fc0d16b8_400x400.jpeg"
         alt="my face">
       <p>
-      <img style="-webkit-user-select: none;" src="http://75.132.59.151:8081//" width="595" height="446">
-      </p>
-      <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa optio doloribus nam libero natus animi
         dolore rerum alias nulla sequi consequatur veniam aliquam suscipit repellendus, dignissimos recusandae quam
         excepturi.
