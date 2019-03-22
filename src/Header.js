48e1b60c76af1f05ceb0d7173877eae8b25fdefc
Dinko Karamfilov <bgllc91@gmail.com>
2019-03-22 02:28:22 -0500

    new h3

--- src/Header.js
+++ src/Header.js
@@ -3,7 +3,7 @@ export default function Header(state){
     <div id="header">
       <div class="container">
         <h1>${state[state.active].title}</h1>
-        <h2>Also hello world</h2>
+        <h2>Feel free to contact me: BgLLC91@gmail.com</h2>
         <h3>one more for good measure</h3>
       </div>
     </div>
--- state/Projects.js
+++ state/Projects.js
@@ -1,5 +1,5 @@
 export default {
     'body': 'Projects',
     'links': [ 'Home', 'Blog', 'Contact' ],
-    'title': 'Feel free to contact me: BgLLC91@gmail.com'
+    'title': 'Some mICROcONTROLLERpROJECTS'
 };
