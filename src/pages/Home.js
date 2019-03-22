6cdb20c7cc20ea1f6ca6687af02db4cd6cb8b9ac
Dinko Karamfilov <bgllc91@gmail.com>
2019-03-22 00:43:25 -0500

    live feed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,7 @@ export default function Home(){
       <img src="https://pbs.twimg.com/profile_images/378800000427609721/c50b7445958e266822842f82fc0d16b8_400x400.jpeg"
         alt="my face">
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sit vel officiis praesentium cupiditate temporibus
-        corrupti quis, architecto odit, aliquam mollitia fugit totam, in ab ut dolores cumque iusto. Ad, facilis.
+      <img style="-webkit-user-select: none;" src="http://192.168.1.4:8081/" width="595" height="446">
       </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa optio doloribus nam libero natus animi
