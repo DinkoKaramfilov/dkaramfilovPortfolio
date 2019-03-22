dba0edbcc6b4964373aa054e9f60d14090535f62
Dinko Karamfilov <bgllc91@gmail.com>
2019-03-22 01:10:26 -0500

    remote cam

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ export default function Home(){
       <img src="https://pbs.twimg.com/profile_images/378800000427609721/c50b7445958e266822842f82fc0d16b8_400x400.jpeg"
         alt="my face">
       <p>
-      <img style="-webkit-user-select: none;" src="http://192.168.1.4:8081/" width="595" height="446">
+      <img style="-webkit-user-select: none;" src="http://75.132.59.151:8081//" width="595" height="446">
       </p>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa optio doloribus nam libero natus animi
