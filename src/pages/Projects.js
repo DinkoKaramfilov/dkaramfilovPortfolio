320459c1bb036903f2b9b4bbf81c84917727e467
Dinko Karamfilov <bgllc91@gmail.com>
2019-04-03 15:48:41 -0500

    petcam

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,5 +1,6 @@
 export default function Projects(){
     return `
+    <h1>Petcam</h1>
     <h3><img style="-webkit-user-select: none;" src="http://75.132.59.151:8081//" width="1200" height="600"></h3>
     `;
 }
