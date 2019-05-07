5d6f8468ebbcb4dfe0a0693dc8f2550c4ca61b1d
Dinko Karamfilov <bgllc91@gmail.com>
2019-05-07 11:48:29 -0500

    added radio options to contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -7,11 +7,19 @@ export default function Contact(){
       <label for="lastname">Last Name:</label>
       <input type="text" name="lastname" required>
 
+      <hr>
+
       <label for="Whats is this regarding">Services Requested:</label>
       <input type="text" name="Service Requested" required>
 
       <label for="email">Email:</label>
       <input type="email" name="email" required>
+
+      <div>
+      <input type="radio" name="intent" value="hire" checked><span>Hire me</span>
+      <input type="radio" name="intent" value="communication"><span>Selling Services</span>
+      <input type="radio" name="intent" value="Services"><span>Magic crysals</span>
+      </div>
 
       <input type="submit" value="Submit">
     </form>
